0317a801e7542a0fb787a4ef3147b2a3015a243c
AoifeWM <aoifewmulligan@gmail.com>
2022-01-15 15:33:32 -0800

    Added functionality for caching movie and weather data to reduce superfluous API calls.

--- movies.js
+++ movies.js
@@ -1,23 +1,35 @@
-'use strict'
+'use strict';
 
 const axios = require('axios');
+const cache = require('./cache.js');
 
 const movies = (req, res) => {
   const cityName = req.query.city || 'Seattle';
+  //Data becomes stale after 24 hours
+  const dataStale = 24*60*60*1000;
 
   const movieAPI = async () => {
+    const key = `movies-${cityName}`;
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`;
 
-    const response = await axios.get(url);
-    console.log(response.data);
-    if (response.data === undefined || response.data.results.length === 0) {
-      console.log('No movies related to this place were found.');
-      res.status(500).send('No movies related to this place were found.');
+    if(cache[key] && (Date.now() - cache[key].timestamp < dataStale)){
+      console.log('Cache hit: Movie data');
+      //console.log(cache[key].data);
+      res.status(200).send(cache[key].data);
     } else {
-      const responseArray = parseMovieData(response.data);
-      res.status(200).send(responseArray);
+      console.log('Cache Miss: movie data');
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
+
+      const response = await axios.get(url);
+      if (response.data === undefined) {
+        res.status(500).send('City not found!');
+      } else {
+        const responseArray = parseMovieData(response.data);
+        res.status(200).send(responseArray);
+        cache[key].data = responseArray;
+      }
     }
-    console.log(response.data.page);
   };
 
   movieAPI();
