d147b8e1872f0cda5b675fec5a391e3a42e826ee
Marni Hager <mlh6118@gmail.com>
2022-01-15 13:54:20 -0800

    Added cache to weather.js

--- weather.js
+++ weather.js
@@ -2,6 +2,7 @@
 
 const axios = require('axios');
 
+let cache = require('./cache.js');
 
 // WEATHER FORECAST API/Forecast Class
 let weatherModule = (req, res) => {
@@ -9,14 +10,26 @@ let weatherModule = (req, res) => {
   const cityLon = req.query.lon || '-122.33207';
   
   const weatherAPI = async () => {
+    const key = 'weather-' + cityLat + cityLon;
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${cityLat}&lon=${cityLon}&units=I&days=5&key=${process.env.WEATHER_API_KEY}`;
     console.log(url);
-    const response = await axios.get(url);
-    if (response.data === undefined) {
-      res.status(500).send('City not found!');
+
+    if(cache[key] && (Date.now() - cache[key].timestamp < 50000)){
+      console.log('Cache hit');
+      res.status(200).send(cache[key].data);
     } else {
-      const responseArray = parseWeatherData(response.data);
-      res.status(200).send(responseArray);
+      console.log('Cache miss');
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
+
+      const response = await axios.get(url);
+      if (response.data === undefined) {
+        res.status(500).send('City not found!');
+      } else {
+        const responseArray = parseWeatherData(response.data);
+        res.status(200).send(responseArray);
+        cache[key].data = response.data;
+      }
     }
   };
 
